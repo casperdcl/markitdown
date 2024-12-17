4bab2f1cd4a9da6b60d02874d60e4960a5a95ce1
Casper da Costa-Luis <casper.dcl@physics.org>
2024-12-17 05:51:30 +0000

    slight tidy
    - follow-up to #46

--- src/markitdown/__main__.py
+++ src/markitdown/__main__.py
@@ -13,36 +13,14 @@ def main():
         description="Convert various file formats to markdown.",
         prog="markitdown",
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        usage=dedent(
-            """
-            SYNTAX:
-
-                markitdown <OPTIONAL: FILENAME>
-                If FILENAME is empty, markitdown reads from stdin.
-
-            EXAMPLE:
-
-                markitdown example.pdf
-
-                OR
-
-                cat example.pdf | markitdown
-
-                OR
-
-                markitdown < example.pdf
-                
-                OR to save to a file use
-    
-                markitdown example.pdf -o example.md
-                
-                OR
-                
-                markitdown example.pdf > example.md
-            """
-        ).strip(),
+        epilog=dedent(
+            """\
+            examples:
+              markitdown example.pdf
+              markitdown -o example.md example.pdf
+              cat example.pdf | markitdown > example.md"""
+        ),
     )
-
     parser.add_argument(
         "-v",
         "--version",
@@ -50,30 +28,27 @@ def main():
         version=f"%(prog)s {__version__}",
         help="show the version number and exit",
     )
-
-    parser.add_argument("filename", nargs="?")
+    parser.add_argument(
+        "filename", nargs="?", help="if unspecified, defaults to stdin"
+    )
     parser.add_argument(
         "-o",
         "--output",
-        help="Output file name. If not provided, output is written to stdout.",
+        dest="filename",
+        help="if unspecified, defaults to stdout",
     )
+
     args = parser.parse_args()
+    markitdown = MarkItDown()
 
     if args.filename is None:
-        markitdown = MarkItDown()
         result = markitdown.convert_stream(sys.stdin.buffer)
-        _handle_output(args, result)
     else:
-        markitdown = MarkItDown()
         result = markitdown.convert(args.filename)
-        _handle_output(args, result)
 
-
-def _handle_output(args, result: DocumentConverterResult):
-    """Handle output to stdout or file"""
     if args.output:
         with open(args.output, "w", encoding="utf-8") as f:
-            f.write(result.text_content)
+            print(result.text_content, file=f)
     else:
         print(result.text_content)
 
