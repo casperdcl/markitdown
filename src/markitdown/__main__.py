73de584526f44e88712a1e7256a0f6397aa940d9
Casper da Costa-Luis <casper.dcl@physics.org>
2024-12-22 08:36:55 +0000

    global parser
    makes external integration with e.g. `shtab` easy

--- src/markitdown/__main__.py
+++ src/markitdown/__main__.py
@@ -7,44 +7,44 @@ from textwrap import dedent
 from .__about__ import __version__
 from ._markitdown import MarkItDown, DocumentConverterResult
 
+parser = argparse.ArgumentParser(
+    description="Convert various file formats to markdown.",
+    prog="markitdown",
+    formatter_class=argparse.RawDescriptionHelpFormatter,
+    epilog=dedent(
+        """\
+        examples:
+          markitdown example.pdf
+          markitdown -o example.md example.pdf
+          cat example.pdf | markitdown > example.md"""
+    ),
+)
+parser.add_argument(
+    "-v",
+    "--version",
+    action="version",
+    version=f"%(prog)s {__version__}",
+    help="show the version number and exit",
+)
+parser.add_argument(
+    "filename", nargs="?", help="if unspecified, defaults to stdin"
+)
+parser.add_argument(
+    "-o",
+    "--output",
+    metavar="outfilename",
+    help="if unspecified, defaults to stdout",
+)
 
-def main():
-    parser = argparse.ArgumentParser(
-        description="Convert various file formats to markdown.",
-        prog="markitdown",
-        formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=dedent(
-            """\
-            examples:
-              markitdown example.pdf
-              markitdown -o example.md example.pdf
-              cat example.pdf | markitdown > example.md"""
-        ),
-    )
-    parser.add_argument(
-        "-v",
-        "--version",
-        action="version",
-        version=f"%(prog)s {__version__}",
-        help="show the version number and exit",
-    )
-    parser.add_argument(
-        "filename", nargs="?", help="if unspecified, defaults to stdin"
-    )
-    parser.add_argument(
-        "-o",
-        "--output",
-        dest="filename",
-        help="if unspecified, defaults to stdout",
-    )
 
-    args = parser.parse_args()
+def main(args=None):
+    args = parser.parse_args(args)
     markitdown = MarkItDown()
 
-    if args.filename is None:
-        result = markitdown.convert_stream(sys.stdin.buffer)
-    else:
+    if args.filename:
         result = markitdown.convert(args.filename)
+    else:
+        result = markitdown.convert_stream(sys.stdin.buffer)
 
     if args.output:
         with open(args.output, "w", encoding="utf-8") as f:
